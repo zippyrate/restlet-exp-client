0e49f6c557433b78d719f814aa07314a38c06408
martin <martin@zippyrate.com>
2013-07-14 15:01:05 -0700

    Inital JSON query and response decode

--- src/main/java/exp/restlet/client/MyClientApplication.java
+++ src/main/java/exp/restlet/client/MyClientApplication.java
@@ -2,6 +2,7 @@ package exp.restlet.client;
 
 import java.io.IOException;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.restlet.Context;
@@ -22,41 +23,86 @@ public class MyClientApplication
 	{
 		Context restletContext = new Context();
 		ClientResource clientResource = new ClientResource(restletContext, "http://localhost:8080/coordination/");
-
-		logger.info("Hello");
+		// Representation request = new JsonRepresentation("{ \"terms\": [] }");
+		Representation request;
 
 		try {
-			Representation request = new JsonRepresentation("{ \"terms\": [] }");
+			request = new JsonRepresentation(createCoordination());
 			request.setMediaType(MediaType.APPLICATION_JSON); // CharacterSet.UTF_8);
 
 			Representation response = clientResource.post(request);
 
 			Status status = clientResource.getStatus();
 			logger.info("SS: " + status);
-			logger.info("RE: " + clientResource.getResponseEntity());
 
-			JsonRepresentation returnedRepresentation = new JsonRepresentation(response);
-
-			if (returnedRepresentation == null)
-				logger.warn("ret = null");
-			else
-				logger.info("X: " + returnedRepresentation.getSize());
-			JSONObject responseObject = returnedRepresentation.getJsonObject();
-
-			// Clean-up
-			request.release();
-			returnedRepresentation.release();
+			decodeResponse(response);
 			response.release();
-			clientResource.release();
+			request.release();
 		} catch (ResourceException e) {
 			Status status = clientResource.getStatus();
 			logger.info("S: " + status);
 			logger.info("RE: " + clientResource.getResponseEntity());
-			logger.info("RE.size: " + clientResource.getResponseEntity().getSize());
+			decodeErrorResponse(clientResource.getResponseEntity());
 		} catch (IOException e) {
 			logger.warn("IOException", e);
 		} catch (JSONException e) {
 			logger.warn("JSONException", e);
+		} finally {
+			clientResource.release();
 		}
+	}
+
+	private static JSONObject createCoordination() throws JSONException
+	{
+		JSONObject requestObject = new JSONObject();
+		JSONArray items = new JSONArray();
+
+		items.put(createTerm("id1", "s", "1.0"));
+		items.put(createTerm("id2", "s", "1.0"));
+
+		requestObject.put("terms", items);
+
+		return requestObject;
+	}
+
+	private static JSONObject createTerm(String termID, String schemaName, String schemaVersion) throws JSONException
+	{
+		JSONObject term = new JSONObject();
+
+		term.put("termID", termID);
+		term.put("schemaName", schemaName);
+		term.put("schemaVersion", schemaVersion);
+		term.put("description", "");
+
+		return term;
+	}
+
+	private static String decodeErrorResponse(Representation responseRepresentation)
+	{
+		try {
+			JsonRepresentation returnedRepresentation = new JsonRepresentation(responseRepresentation);
+			JSONObject responseObject = returnedRepresentation.getJsonObject();
+			String error = responseObject.getString("error");
+
+			responseRepresentation.release();
+			return error;
+		} catch (Exception e) {
+			logger.warn("Bad JSON error response", e);
+			return "Bad JSON error response";
+		}
+	}
+
+	private static void decodeResponse(Representation responseRepresentation) throws JSONException, IOException
+	{
+		JsonRepresentation returnedRepresentation = new JsonRepresentation(responseRepresentation);
+		JSONObject responseObject = returnedRepresentation.getJsonObject();
+		String termID = responseObject.getString("termID");
+		String schemaName = responseObject.getString("schemaName");
+		String schemaVersion = responseObject.getString("schemaVersion");
+		String description = responseObject.getString("description");
+		responseRepresentation.release();
+
+		logger.info("termID " + termID + "termID " + termID + ", schemaName " + schemaName + ", schemaVersion "
+				+ schemaVersion + ", description " + description);
 	}
 }
